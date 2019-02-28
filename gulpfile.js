e495cf94f2a4a2b9829a8c31a1bc088870511b16
Sergey Platicin <s.platicin@iMac2.local>
2019-02-28 17:08:18 +0300

    Small fixes, added default media queries in style.sass

--- gulpfile.js
+++ gulpfile.js
@@ -138,6 +138,7 @@ gulp.task('default', ['browserSync', 'sass'], function() {
 	gulp.watch('app/sass/**/*.sass', ['sass']);
 	gulp.watch('app/css/**/*.css', browserSync.reload);
 	gulp.watch('app/*.html', browserSync.reload);
+	gulp.watch('app/templates/*.html', browserSync.reload);
 	gulp.watch('app/js/**/*.js', browserSync.reload);
 });
 
